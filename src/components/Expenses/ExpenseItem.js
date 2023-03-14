f0ff3d4f6feec1b395de2728854a8c7e4c70f266
Cliffherr312 <113385169+Cliffherr312@users.noreply.github.com>
2023-03-14 00:07:21 -0400

    3/13 React Course

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,21 +1,30 @@
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
-import React from "react";
+import React, { useState } from "react"; //Need to load useState to update Screen with button presses
 import "./ExpenseItem.css";
-
 
 const ExpenseItem = (props) => {
   //props allows for the trasfer of data from different components
 
+  const [title, setTitle] = useState(props.title); //Needed for button press to update Screen([FirstVarible is starting,SecondVariable is Final state])
+  console.log("Expense Item Evaluated by React");
+
+  const clickHandler = () => {
+    //Button Click Handler
+    setTitle("Updated!");
+    console.log(title);
+  };
+
   return (
     <Card className="expense-item">
-      <ExpenseDate date={props.date}/>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">{props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
-}
+};
 
 export default ExpenseItem;
